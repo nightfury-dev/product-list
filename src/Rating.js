0ee57555ffb9a5f1d335b82d3f4d64bbdd75beb2
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-06-02 18:46:20 -0700

    add Glyphicons for star ratings

--- src/Rating.js
+++ src/Rating.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Glyphicon } from 'react-bootstrap';
 
 class Rating extends Component {
   render() {
     return(
       <div>
         <h1>Rating: {this.props.rating}</h1>
+        <Glyphicon
+          glyph={this.props.rating >= 1 ? "star" : "star-empty"}
+        />
+        <Glyphicon
+          glyph={this.props.rating >= 2 ? "star" : "star-empty"}
+        />
+        <Glyphicon
+          glyph={this.props.rating >= 3 ? "star" : "star-empty"}
+        />
+        <Glyphicon
+          glyph={this.props.rating >= 4 ? "star" : "star-empty"}
+        />
+        <Glyphicon
+          glyph={this.props.rating >= 5 ? "star" : "star-empty"}
+        />
       </div>
     );
   }
