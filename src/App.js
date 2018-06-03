173fd449590fa84254d11158d20649ff31077156
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-06-02 18:36:10 -0700

    disable button with flag

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
+
+    const isValid = true;
+
     return (
       <div>
         <h1>Another React Tutorial!</h1>
         <Products />
-        <Button bsStyle="primary">Default</Button>
+        <Button bsStyle="primary" disabled={!isValid}>Default</Button>
       </div>
     );
   }
