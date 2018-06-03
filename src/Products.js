20b4014a369a9e740e1e4ec2486562476bb397b4
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-06-02 17:56:41 -0700

    add and display product list

--- src/Products.js
+++ src/Products.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 
 class Products extends Component {
   render () {
+
+    const products = ["Learning React", "Pro React", "Beginning React"];
+    const listProducts = products.map((product) =>
+      <li key={product.toString()}>{product}</li>
+    );
+
     return (
       <div>
         <h2>
           Products
         </h2>
+        <ul>{listProducts}</ul>
       </div>
     );
   }
