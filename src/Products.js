b02f4884014643a96c8894a294736de5faa3932a
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-06-02 17:41:06 -0700

    create Products component

---
+++ src/Products.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+  render () {
+    return (
+      <div>
+        <h2>
+          Products
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default Products;
