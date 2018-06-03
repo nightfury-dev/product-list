2548e1eeaf3c3c4c0b6d10e143dc05cc3222f182
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-06-02 17:43:05 -0700

    import Products component into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Products from './Products';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Another React Tutorial!</h1>
+        <Products />
       </div>
     );
   }
