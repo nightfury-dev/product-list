d6e7ddaa0944667ea37740c65a36e32e20f21b77
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-06-02 18:42:54 -0700

    import Rating component; add series of Rating elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Products from './Products';
 import { Button } from 'react-bootstrap';
+import Rating from './Rating';
 
 class App extends Component {
   render() {
@@ -11,6 +12,11 @@ class App extends Component {
       <div>
         <h1>Another React Tutorial!</h1>
         <Products />
+        <Rating rating="1" />
+        <Rating rating="2" />
+        <Rating rating="3" />
+        <Rating rating="4" />
+        <Rating rating="5" />
         <Button bsStyle="primary" disabled={!isValid}>Default</Button>
       </div>
     );
