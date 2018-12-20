18d90c6679e2286b097e054c25c15d48a5466c2d
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-12-20 12:54:08 -0800

    add constructor; get product data

--- src/Products.js
+++ src/Products.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.products = this.getProducts();
+  }
 
   getProducts() {
     return [
