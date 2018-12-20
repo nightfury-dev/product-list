3cfbba06c0212e68e6ce152e5cb86eb54ff1bbd7
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-12-19 22:56:24 -0800

    add data props to Product media object

--- src/Product.js
+++ src/Product.js
@@ -10,15 +10,19 @@ class Product extends Component {
       <div>
         <Media>
           <Media.Left>
-            <img width={64} height={64} src="/thumbnail.png" alt="thumbnail" />
+            <img width={64} height={64} src={this.props.data.imageUrl} alt="image" />
           </Media.Left>
           <Media.Body>
-            <Media.Heading>Media Heading</Media.Heading>
+            <Media.Heading>{this.props.data.productName}</Media.Heading>
             <p>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-              ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-              tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-              fringilla. Donec lacinia congue felis in faucibus.
+              {this.props.releasedDate}
+            </p>
+            <Rating
+              rating={this.props.data.rating}
+              numOfReviews={this.props.data.numOfReviews}
+            />
+            <p>
+              {this.props.data.description}
             </p>
           </Media.Body>
         </Media>
