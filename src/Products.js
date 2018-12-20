b4bcda95be8955a9d959306ec16adf903e110ea6
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-12-20 12:40:49 -0800

    add getProducts() method with product data

--- src/Products.js
+++ src/Products.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 
 class Products extends Component {
+
+  getProducts() {
+    return [
+    {
+        imageUrl: "http://loremflickr.com/150/150?random=1",
+        productName: "Product 1",
+        releasedDate: "May 31, 2016",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porttitor, tellus laoreet venenatis facilisis, enim ex faucibus nulla, id rutrum ligula purus sit amet mauris. ",
+        rating: 4,
+        numOfReviews: 2
+    },
+    {
+        imageUrl: "http://loremflickr.com/150/150?random=2",
+        productName: "Product 2",
+        releasedDate: "October 31, 2016",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porttitor, tellus laoreet venenatis facilisis, enim ex faucibus nulla, id rutrum ligula purus sit amet mauris. ",
+        rating: 2,
+        numOfReviews: 12
+    },
+    {
+        imageUrl: "http://loremflickr.com/150/150?random=3",
+        productName: "Product 3",
+        releasedDate: "July 30, 2016",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porttitor, tellus laoreet venenatis facilisis, enim ex faucibus nulla, id rutrum ligula purus sit amet mauris. ",
+        rating: 5,
+        numOfReviews: 2
+    }];
+  }
+
   render () {
 
     const products = ["Learning React", "Pro React", "Beginning React"];
