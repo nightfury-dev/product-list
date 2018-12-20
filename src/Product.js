48f00b05c03b517e6ab7a0d440ab3f325f7115f6
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-12-19 22:44:23 -0800

    render generic bootstrap media object

--- src/Product.js
+++ src/Product.js
@@ -7,7 +7,22 @@ class Product extends Component {
 
   render() {
     return(
-
+      <div>
+        <Media>
+          <Media.Left>
+            <img width={64} height={64} src="/thumbnail.png" alt="thumbnail" />
+          </Media.Left>
+          <Media.Body>
+            <Media.Heading>Media Heading</Media.Heading>
+            <p>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+              ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+              tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+              fringilla. Donec lacinia congue felis in faucibus.
+            </p>
+          </Media.Body>
+        </Media>
+      </div>;
     );
   }
 }
